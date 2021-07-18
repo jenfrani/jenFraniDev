df32e2e64b7681a6fbadc35a624f5a8e9db0b2c3
jenFrani <jeneelfrani@gmail.com>
2021-07-17 19:04:20 -0600

    Changed WP url.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "jFraniCode",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-wordpress",
-      options: {
-        url: "jfranicodes.local",
-      },
-    },
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+	siteMetadata: {
+		siteUrl: "https://www.yourdomain.tld",
+		title: "jFraniCode",
+	},
+	plugins: [
+		{
+			resolve: "gatsby-source-wordpress",
+			options: {
+				url: "http://jfranicodes.local/graphql",
+			},
+		},
+		"gatsby-plugin-styled-components",
+		"gatsby-plugin-gatsby-cloud",
+		"gatsby-plugin-image",
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+	],
 };
